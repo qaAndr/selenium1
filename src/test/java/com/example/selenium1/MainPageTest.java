9be4a2e4ced5cb4b078165cac356c700926e687b
Андрей <bentosbig@gmail.com>
2023-07-12 14:01:07 +0300

    Test open site added

--- src/test/java/com/example/selenium1/MainPageTest.java
+++ src/test/java/com/example/selenium1/MainPageTest.java
@@ -9,8 +9,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -41,5 +45,25 @@ public class MainPageTest {
 
         WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
         assertEquals(input, searchPageField.getAttribute("value"));
+    }
+
+    @Test
+    public void openWebsite() {
+        String input = "Selenium";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys(input);
+        searchField.submit();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]")));
+        List<WebElement> results = driver.findElements(By.cssSelector("h2 > a[href]"));
+        clickElement(results, 0);
+        ArrayList<String> tabs = new ArrayList<> (driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        driver.getCurrentUrl();
+        assertEquals("https://www.selenium.dev/", driver.getCurrentUrl(), "не тот сайт" );
+    }
+    public void clickElement(List<WebElement> results, int num){
+        results.get(num).click();
+        System.out.println("Произведен клик");
     }
 }
